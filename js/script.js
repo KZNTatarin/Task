c6ae46951b92504dfcc3404822e97cf131dfbad1
Amir Kadyrov <amirwork976@gmail.com>
2022-10-28 19:45:07 +0300

    Удаление по ID

--- js/script.js
+++ js/script.js
@@ -1,55 +1,51 @@
 const openCreat = () => {
-    const creatTaskBlock = document.querySelector('.creat-task');
-    creatTaskBlock.classList.toggle('active');
-}
+  const creatTaskBlock = document.querySelector(".creat-task");
+  creatTaskBlock.classList.toggle("active");
+};
 
 const creatTask = () => {
-    let header = document.querySelector('#header').value;
-    let text = document.querySelector('#text').value;
-    const taskWrapp = document.querySelector('.tasks__wrapp');
+  let header = document.querySelector("#header").value;
+  let text = document.querySelector("#text").value;
+  const taskWrapp = document.querySelector(".tasks__wrapp");
 
-    let task = document.createElement('div');
-    task.className = 'task';
+  let task = document.createElement("div");
+  task.className = "task";
 
-    const id = Math.floor(Math.random() * 10000000);
-    task.setAttribute("id", String(id));
+  const id = Math.floor(Math.random() * 10000000);
+  task.setAttribute("id", String(id));
 
-    let taskChanse = document.createElement('button');
-    taskChanse.className = 'btn task__chanse';
-    taskChanse.innerHTML = 'Изменить'
+  let taskChanse = document.createElement("button");
+  taskChanse.className = "btn task__chanse";
+  taskChanse.innerHTML = "Изменить";
 
-    let taskDelete = document.createElement('button');
-    taskDelete.className = 'btn task__delete';
-    taskDelete.innerHTML = 'Удалить';
+  let taskDelete = document.createElement("button");
+  taskDelete.className = "btn task__delete";
+  taskDelete.innerHTML = "Удалить";
+  taskDelete.setAttribute("onClick", `deleteTask(${id})`);
 
-    let taskHeader = document.createElement('h2');
-    taskHeader.innerHTML = header;
+  let taskHeader = document.createElement("h2");
+  taskHeader.innerHTML = header;
 
-    let taskText = document.createElement('p');
-    taskText.innerHTML = text;
-    
-    task.appendChild(taskHeader);
-    task.appendChild(taskText);
-    task.appendChild(taskChanse);
-    task.appendChild(taskDelete);
-    taskWrapp.appendChild(task);
+  let taskText = document.createElement("p");
+  taskText.innerHTML = text;
 
-    header = document.querySelector('#header').value = '';
-    text = document.querySelector('#text').value = '';
-}
+  task.appendChild(taskHeader);
+  task.appendChild(taskText);
+  task.appendChild(taskChanse);
+  task.appendChild(taskDelete);
+  taskWrapp.appendChild(task);
 
+  header = document.querySelector("#header").value = "";
+  text = document.querySelector("#text").value = "";
+};
 
-const deleteTask = (task) => {
-    let deleteBtn = document.querySelectorAll('.task__delete');
-    deleteBtn.addEventListener('click', function() {
-        taskWrapp.removeChild(task);
-    })
-}
+const deleteTask = (id) => {
+  let wrapper = document.querySelector(".tasks__wrapp");
 
+  const askUser = confirm("Удалить задачу?");
 
-
-
-
-
-
-
+  if (askUser === true) {
+    const elementToDelete = document.getElementById(`${id}`);
+    wrapper.removeChild(elementToDelete);
+  }
+};
